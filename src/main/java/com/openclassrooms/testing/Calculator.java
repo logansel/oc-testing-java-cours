5a7ea89818936e71ed1086e38cb7973efc305b6d
logan <logansel59162@gmail.com>
2022-08-01 19:30:34 +0200

    IntelJ2

--- src/main/java/com/openclassrooms/testing/Calculator.java
+++ src/main/java/com/openclassrooms/testing/Calculator.java
@@ -5,31 +5,31 @@ import java.util.Set;
 
 public class Calculator {
 
-	public int add(int a, int b) {
-		return a + b;
-	}
+    public int add(int a, int b) {
+        return a + b;
+    }
 
-	public int multiply(int a, int b) {
-		return a * b;
-	}
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 
-	public void longCalculation() {
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public void longCalculation() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     public Set<Integer> digitsSet(int number) {
-		final Set<Integer> integers = new HashSet<Integer>();
-		final String numberString = String.valueOf(number);
+        final Set<Integer> integers = new HashSet<Integer>();
+        final String numberString = String.valueOf(number);
 
-		for(int i =0; i< numberString.length(); i++){
-			if(numberString.charAt(i) != '-'){
-				integers.add(Integer.parseInt(numberString,i,i+1,10));
-			}
-		}
-		return integers;
+        for (int i = 0; i < numberString.length(); i++) {
+            if (numberString.charAt(i) != '-') {
+                integers.add(Integer.parseInt(numberString, i, i + 1, 10));
+            }
+        }
+        return integers;
     }
 }
