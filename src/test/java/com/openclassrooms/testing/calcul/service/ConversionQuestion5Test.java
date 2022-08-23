e3b6cd2c8f13079f605b228739ae282e92dcf4fc
logan <logansel59162@gmail.com>
2022-08-23 14:23:35 +0200

    ree

---
+++ src/test/java/com/openclassrooms/testing/calcul/service/ConversionQuestion5Test.java
@@ -0,0 +1,41 @@
+package com.openclassrooms.testing.calcul.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+
+import com.openclassrooms.testing.calcul.domain.ConversionCalculator;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ConversionQuestion5Test {
+    @Mock
+    ConversionCalculator calculator;
+
+    ConversionCalculatorServiceImpl conversionCalculatorService;
+
+    @BeforeEach
+    public void init() {
+        conversionCalculatorService = new ConversionCalculatorServiceImpl(calculator);
+    }
+
+    @Test
+    public void calculate_shouldUseCalculator_forCelsiusToFarenheit() {
+        // GIVEN
+        when(calculator.celsiusToFahrenheit(0.0)).thenReturn(32.);
+        final double resultatAttendu = 32.;
+
+        // WHEN
+        final double resultat = conversionCalculatorService.calculate(new ConversionModel(0.0, ConversionType.CELSIUS_TO_FARENHEIT)).getSolution();
+
+        // THEN
+        verify(calculator).celsiusToFahrenheit(0.0);
+        assertThat(resultat).isEqualTo(resultatAttendu);
+
+    }
+}
